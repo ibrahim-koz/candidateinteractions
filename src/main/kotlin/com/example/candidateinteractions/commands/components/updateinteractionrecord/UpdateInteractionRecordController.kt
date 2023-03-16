b1905e71df15a1cf18e87e89b630d0e405475749
ibrahimkoz <ibrahimkoz.cs@gmail.com>
2023-03-16 13:44:25 +0300

    Completed controller of UpdateInteractionRecordController

--- src/main/kotlin/com/example/candidateinteractions/commands/components/updateinteractionrecord/UpdateInteractionRecordController.kt
+++ src/main/kotlin/com/example/candidateinteractions/commands/components/updateinteractionrecord/UpdateInteractionRecordController.kt
@@ -1,5 +1,38 @@
 package com.example.candidateinteractions.commands.components.updateinteractionrecord
 
-class UpdateInteractionRecordController {
+import com.example.candidateinteractions.queries.InteractionRecordRepresentation
+import com.example.candidateinteractions.queries.QueryService
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 
+data class UpdateInteractionRecordRequest(
+    val interactionMethod: String,
+    val phoneNumberOfInterviewer: String? = null,
+    val emailOfInterviewer: String? = null
+)
+
+@RestController
+class UpdateInteractionRecordController(
+    private val updateInteractionRecordHandler: UpdateInteractionRecordHandler,
+    private val queryService: QueryService
+) {
+    @PutMapping("candidate/{candidateId}/interaction-record/{interactionRecordId}")
+    fun handle(
+        @PathVariable candidateId: String,
+        @PathVariable interactionRecordId: String,
+        @RequestBody request: UpdateInteractionRecordRequest
+    ): InteractionRecordRepresentation {
+        updateInteractionRecordHandler.handle(
+            UpdateInteractionRecordParams(
+                scalarCandidateId = candidateId,
+                scalarInteractionRecordId = interactionRecordId,
+                scalarInteractionMethod = request.interactionMethod,
+                scalarPhoneNumberOfInterviewer = request.phoneNumberOfInterviewer,
+                scalarEmailOfInterviewer = request.emailOfInterviewer
+            )
+        )
+        return queryService.getInteractionRecord(scalarId = interactionRecordId)
+    }
 }
