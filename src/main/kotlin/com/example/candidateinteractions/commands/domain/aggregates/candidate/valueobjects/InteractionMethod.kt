1cf5c84f7d6df30148c4e3396329450f5bbdde57
ibrahimkoz <ibrahimkoz.cs@gmail.com>
2023-03-21 19:41:13 +0300

    Made InteractionMethod case insensitive

--- src/main/kotlin/com/example/candidateinteractions/commands/domain/aggregates/candidate/valueobjects/InteractionMethod.kt
+++ src/main/kotlin/com/example/candidateinteractions/commands/domain/aggregates/candidate/valueobjects/InteractionMethod.kt
@@ -3,14 +3,14 @@ package com.example.candidateinteractions.commands.domain.aggregates.candidate.v
 class InvalidInteractionMethodException(val value: String) : IllegalArgumentException()
 
 enum class InteractionMethod(val value: String) {
-    PHONE_INTERACTION("PhoneInteraction"),
-    EMAIL_INTERACTION("EmailInteraction");
+    PHONE_INTERACTION("phoneinteraction"),
+    EMAIL_INTERACTION("emailinteraction");
 
     companion object {
         private val valueMap = values().associateBy(InteractionMethod::value)
 
         fun fromValue(value: String): InteractionMethod =
-            valueMap[value] ?: throw InvalidInteractionMethodException(value)
+            valueMap[value.lowercase()] ?: throw InvalidInteractionMethodException(value)
     }
 }
 
